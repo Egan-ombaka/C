e438cccd4817c8b72ccd1255b0ef232f15d7ace3
Egan-ombaka <169147080+Egan-ombaka@users.noreply.github.com>
2024-05-22 15:36:03 +0300

    Add files via upload

---
+++ Assignment.c.c
@@ -0,0 +1,17 @@
+// first c assignment
+/*
+First c assignment
+By: Egan Ombaka
+Date: 17/05/2034
+*/
+
+#include<stdio.h>
+int main(){
+    printf(" Egan \n ");
+    printf("zetech univesity \n ");
+    printf("Invent your future \n ");
+    printf("now \n ");
+    printf(" WELCOME TO ZETECH");
+
+    return 0;
+}
